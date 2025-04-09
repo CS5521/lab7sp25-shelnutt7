12dc622fc57bcf17dc04e2044940eb8ca522c750
Nicholas Shelnutt <shelnuttnq@appstate.edu>
2025-04-09 15:14:08 +0000

    added ticks and tickets to proc struct

--- proc.h
+++ proc.h
@@ -49,6 +49,8 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  int tickets;		       // number of tickets a process has
+  int ticks;		       // number of tickets each process has accumulated
 };
 
 // Process memory is laid out contiguously, low addresses first:
